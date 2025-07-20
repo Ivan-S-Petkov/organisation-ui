d9ba7e3f5ab852a198e29843648a7588070ae761
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-07-21 01:15:21 +0300

    Implemented routing, user authentication, and styled layout in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Header from './components/Header';
+import LoginModal from './components/LoginModal';
+import { Toaster } from 'react-hot-toast';
+import { Route, Routes } from 'react-router-dom';
+import PlanPage from './components/PlanPage';
+import UsersPage from './pages/UsersPage/UsersPage';
+import CreateUserPage from './pages/CreateUserPage/CreateUserPage';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Body>
+      <Toaster position="top-right" />
+      <Header onLoginToggle={() => setIsLoginOpen(true)} />
+      <LoginModal
+        open={isLoginOpen}
+        onClose={() => setIsLoginOpen(false)}
+      />
+      <Main>
+        <Routes>
+          <Route path="/" element={<UsersPage />} />
+          <Route path="/plan"
+            element={<ProtectedRoute requiredRole='user'>
+              <PlanPage />
+            </ProtectedRoute>} />
+          <Route path="/admin">
+            <Route path='user/create'
+              element={<ProtectedRoute requiredRole="admin">
+                <CreateUserPage />
+              </ProtectedRoute>}
+            />
+          </Route>
+        </Routes>
+      </Main>
+    </Body>
   );
 }
+
+const Body = styled.div`
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-color: #f4f4f4;
+  color: #333;
+`;
+
+const Main = styled.main`
+  padding: 10px 20px;
+`;
+
 
 export default App;
