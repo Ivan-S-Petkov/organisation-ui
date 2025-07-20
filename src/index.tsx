ada42b3dbe362c8e057a6cb39fa0502cc1624920
Ivan Petkov <ivan.slv.petkov@gmail.com>
2025-07-21 01:14:22 +0300

    Wrap App component with BrowserRouter, QueryClientProvider and UserProvider

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { UserProvider } from './context/UserContext';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
